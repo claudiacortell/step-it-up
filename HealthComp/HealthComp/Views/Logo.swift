9ad33f78b516a9f1b3749beb79afad09566835cd
philluple <pnl2111@columbia.edu>
2023-11-07 16:29:16 -0500

    added the colors and logo

---
+++ HealthComp/HealthComp/Views/Logo.swift
@@ -0,0 +1,22 @@
+//
+//  Logo.swift
+//  HealthComp
+//
+//  Created by Phillip Le on 11/7/23.
+//
+
+import SwiftUI
+
+struct Logo: View {
+    var body: some View {
+        Image(systemName: "heart.circle.fill")
+            .resizable()
+            .foregroundColor(Color("dark-blue"))
+            .frame(width: 150, height: 150)
+        
+    }
+}
+
+#Preview {
+    Logo()
+}
