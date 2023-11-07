a85167383486b85d65d9d3532c7c3e1c29e17a02
philluple <pnl2111@columbia.edu>
2023-11-07 11:55:27 -0500

    added the correct files

---
+++ HealthComp/HealthComp/Views/CompDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  CompDetailView.swift
+//  HealthComp
+//
+//  Created by Phillip Le on 11/7/23.
+//
+
+import SwiftUI
+
+struct CompDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompDetailView()
+}
---
+++ HealthComp/HealthComp/Views/LeaderboardView.swift
@@ -0,0 +1,18 @@
+//
+//  LeaderboardView.swift
+//  HealthComp
+//
+//  Created by Phillip Le on 11/7/23.
+//
+
+import SwiftUI
+
+struct LeaderboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LeaderboardView()
+}
