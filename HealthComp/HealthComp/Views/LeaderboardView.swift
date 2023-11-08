3b9cef9ac9e3fc4d0202a7a18bbc16f26213da05
claudiacortell <cccortell@gmail.com>
2023-11-08 12:49:56 -0500

    a small start to the LeaderboardView

--- HealthComp/HealthComp/Views/LeaderboardView.swift
+++ HealthComp/HealthComp/Views/LeaderboardView.swift
@@ -9,10 +9,54 @@ import SwiftUI
 
 struct LeaderboardView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            ScrollView{
+                Header().padding(.bottom, 5)
+                LeaderboardMessage()
+                LeaderboardCell()
+                LeaderboardCell()
+                LeaderboardCell()
+                LeaderboardCell()
+                LeaderboardCell()
+                LeaderboardCell()
+            }
+        }
+    }
+}
+    
+#Preview {
+    LeaderboardView()
+}
+    
+struct Header: View {
+    var body: some View {
+        ZStack{
+            Color("medium-green")
+                .ignoresSafeArea()
+            Text("Leaderboard").font(.system(size: 30, weight: .semibold)).foregroundColor(Color("dark-blue"))
+        }
+        Text("Weekly Steps").font(.system(size: 20, weight: .semibold)).foregroundColor(Color("dark-blue"))
     }
 }
 
-#Preview {
-    LeaderboardView()
+struct LeaderboardMessage: View {
+    let message = "You're on top! Keep it up!"
+    var body: some View {
+        Text(message).font(.system(size: 16, weight: .semibold)).foregroundColor(Color("dark-blue"))
+    }
+}
+
+struct LeaderboardCell: View{
+    var body: some View{
+        ZStack{
+            RoundedRectangle(cornerRadius: 10.0)
+                                .frame(width:UIScreen.main.bounds.width-40, height: 60)
+                                .foregroundColor(Color("light-green"))
+
+            HStack {
+                Text("You").padding(.trailing, 100)
+                Text("12356")
+            }
+        }
+    }
 }
