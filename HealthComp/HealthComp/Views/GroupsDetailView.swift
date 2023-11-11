031c554054b7d62d5993a4e0c95b438498c15d1e
eborna3353 <eb3353@columbia.edu>
2023-11-11 11:25:31 -0500

    fixed profile view so that it uses the user structs and aligns better with our aesthetics

---
+++ HealthComp/HealthComp/Views/GroupsDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  GroupsDetailView.swift
+//  HealthComp
+//
+//  Created by Eugenia Bornacini on 11/11/23.
+//
+
+import SwiftUI
+
+struct GroupsDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupsDetailView()
+}
---
+++ HealthComp/HealthComp/Views/GroupsView.swift
@@ -0,0 +1,18 @@
+//
+//  GroupsView.swift
+//  HealthComp
+//
+//  Created by Eugenia Bornacini on 11/11/23.
+//
+
+import SwiftUI
+
+struct GroupsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupsView()
+}
