644e218b964de632e716fe53b3ec6e4806acce15
Teodora Sutilovic <teodorasutilovic@Teodoras-MacBook-Air.local>
2023-11-11 12:09:53 -0500

    working

---
+++ HealthComp/HealthComp/Views/FriendSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  FriendSearchView.swift
+//  HealthComp
+//
+//  Created by Teodora Sutilovic on 11/11/23.
+//
+
+import SwiftUI
+
+struct FriendSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FriendSearchView()
+}
